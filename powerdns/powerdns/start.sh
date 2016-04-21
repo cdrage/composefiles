79917181d25e12a7ffbda17d869a27ce5d447328
Charlie Drage <charlie@charliedrage.com>
2016-04-20 21:28:12 -0400

    Add powerdns

---
+++ powerdns/powerdns/start.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+MYSQL_HOST=${MYSQL_HOST}
+MYSQL_USER=root
+MYSQL_PASSWORD=${MYSQL_ROOT_PASSWORD}
+MYSQL_DB=pdns
+PDNS_ALLOW_AXFR_IPS=${PDNS_ALLOW_AXFR_IPS:-127.0.0.1}
+PDNS_MASTER=${PDNS_MASTER:-yes}
+PDNS_SLAVE=${PDNS_SLAVE:-no}
+PDNS_CACHE_TTL=${PDNS_CACHE_TTL:-20}
+PDNS_DISTRIBUTOR_THREADS=${PDNS_DISTRIBUTOR_THREADS:-3}
+PDNS_RECURSIVE_CACHE_TTL=${PDNS_RECURSIVE_CACHE_TTL:-10}
+POWERADMIN_HOSTMASTER=${POWERADMIN_HOSTMASTER:-}
+POWERADMIN_NS1=${POWERADMIN_NS1:-}
+POWERADMIN_NS2=${POWERADMIN_NS2:-}
+
+until nc -z ${MYSQL_HOST} 3306; do
+    echo "$(date) - waiting for mysql..."
+    sleep 1
+done
+
+if mysql -u root -p${MYSQL_PASSWORD} --host=${MYSQL_HOST} "${MYSQL_DB}" >/dev/null 2>&1 </dev/null
+then
+	echo "Database ${MYSQL_DB} already exists"
+else
+	mysql -u root -p${MYSQL_PASSWORD} --host=${MYSQL_HOST} -e "CREATE DATABASE ${MYSQL_DB}"
+	mysql -u root -p${MYSQL_PASSWORD} --host=${MYSQL_HOST} pdns < /pdns.sql
+	mysql -u root -p${MYSQL_PASSWORD} --host=${MYSQL_HOST} pdns < /poweradmin.sql
+	rm /pdns.sql /poweradmin.sql
+fi
+
+### PDNS
+sed -i "s/{{MYSQL_HOST}}/${MYSQL_HOST}/" /etc/powerdns/pdns.d/pdns.local.gmysql.conf
+sed -i "s/{{MYSQL_USER}}/${MYSQL_USER}/" /etc/powerdns/pdns.d/pdns.local.gmysql.conf
+sed -i "s/{{MYSQL_PASSWORD}}/${MYSQL_PASSWORD}/" /etc/powerdns/pdns.d/pdns.local.gmysql.conf
+sed -i "s/{{MYSQL_DB}}/${MYSQL_DB}/" /etc/powerdns/pdns.d/pdns.local.gmysql.conf
+sed -i "s/{{PDNS_ALLOW_AXFR_IPS}}/${PDNS_ALLOW_AXFR_IPS}/" /etc/powerdns/pdns.conf
+sed -i "s/{{PDNS_MASTER}}/${PDNS_MASTER}/" /etc/powerdns/pdns.conf
+sed -i "s/{{PDNS_SLAVE}}/${PDNS_SLAVE}/" /etc/powerdns/pdns.conf
+sed -i "s/{{PDNS_CACHE_TTL}}/${PDNS_CACHE_TTL}/" /etc/powerdns/pdns.conf
+sed -i "s/{{PDNS_DISTRIBUTOR_THREADS}}/${PDNS_DISTRIBUTOR_THREADS}/" /etc/powerdns/pdns.conf
+sed -i "s/{{PDNS_RECURSIVE_CACHE_TTL}}/${PDNS_RECURSIVE_CACHE_TTL}/" /etc/powerdns/pdns.conf
+
+### POWERADMIN
+sed -i "s/{{MYSQL_HOST}}/${MYSQL_HOST}/" /var/www/html/inc/config.inc.php
+sed -i "s/{{MYSQL_USER}}/${MYSQL_USER}/" /var/www/html/inc/config.inc.php
+sed -i "s/{{MYSQL_PASSWORD}}/${MYSQL_PASSWORD}/" /var/www/html/inc/config.inc.php
+sed -i "s/{{MYSQL_DB}}/${MYSQL_DB}/" /var/www/html/inc/config.inc.php
+sed -i "s/{{POWERADMIN_HOSTMASTER}}/${POWERADMIN_HOSTMASTER}/" /var/www/html/inc/config.inc.php
+sed -i "s/{{POWERADMIN_NS1}}/${POWERADMIN_NS1}/" /var/www/html/inc/config.inc.php
+sed -i "s/{{POWERADMIN_NS2}}/${POWERADMIN_NS2}/" /var/www/html/inc/config.inc.php
+
+exec /usr/bin/supervisord
