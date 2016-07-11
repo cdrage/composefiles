e49ab8cfd700d72a06ec09698bd57965820154b7
Charlie Drage <charlie@charliedrage.com>
2016-07-11 12:08:32 -0400

    update script to download sensu ssl

--- sensu-server/gen_ssl.sh
+++ sensu-server/gen_ssl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir ssl
 cd ssl
-wget http://sensuapp.org/docs/0.21/files/sensu_ssl_tool.tar
+wget http://sensuapp.org/docs/0.23/files/sensu_ssl_tool.tar
 tar -xvf sensu_ssl_tool.tar --strip-components=1
 ./ssl_certs.sh generate
 rm -r sensu_ssl_tool.tar ssl_certs.sh
